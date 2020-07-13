b28efc845961bfd409a8daae299a7651c103c2dc
Jamie Stewart <stwjme@gmail.com>
2020-07-13 19:29:51 -0400

    fibonaccigen

--- hitCheckarray.js
+++ hitCheckarray.js
@@ -1,18 +1,21 @@
+let output = [];
+let count = 1;
+
 function hitCheck () {
 
-  while(count <= 100)
+    for (let count = 1; count < 101; count++) {
 
-  if (count % 3 === 0 && count % 5 === 0) {
-      output.push("hitCheck")
-  }else if (count % 3 === 0) {
-      output.push("Hit");
-  }else if (count % 5 === 0) {
-      output.push("Check");
-  }else {
-      output.push(count);
-  }
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("hitCheck");
+    }else if (count % 3 === 0) {
+        output.push("Hit");
+    }else if (count % 5 === 0) {
+        output.push("Check");
+    }else {
+        output.push(count);
+    }
 
-      count++;
 }
 
 console.log(output);
+}
